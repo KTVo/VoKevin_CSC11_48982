d3fa294b6e4ee42a5c727e47806e8f18b8950c30
kv2394221 <kvo8@student.rcc.edu>
2015-09-11 23:44:05 -0700

    Program 3b Part 1 & 2

---
+++ Hmwk/Assignment 1/part1.s
@@ -0,0 +1,14 @@
+/*
+	Kevin Vo
+	Sept. 11th, 2015
+	Assignment 1
+	Prob: Program 3b
+	Course: CSC11 (48982)
+*/
+
+/* prot1.s file		*/
+	.global _start
+
+_start:
+	MOV R0, #65
+	BAL _part2 @ branches to label part2
---
+++ Hmwk/Assignment 1/part2.s
@@ -0,0 +1,15 @@
+/*
+	Kevin Vo
+	Sept. 11th, 2015
+	Assignment 1
+	Prob: Program 3b
+	Course: CSC11 (48982)
+*/
+
+/* Part2.s file	*/
+
+
+	.global _part2
+_part2:
+	MOV R7, #1
+	SWI 0
