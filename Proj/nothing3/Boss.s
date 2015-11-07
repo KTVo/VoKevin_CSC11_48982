72dcb62b9cb99cf438c7de4d26ef58f0251f8d9c
root <root@raspberrypi.(none)>
2015-11-06 18:51:48 -0800

    yup

--- Proj/nothing3/Boss.s
+++ Proj/nothing3/Boss.s
@@ -4,9 +4,17 @@
 PromptBoss: .asciz "\nAs expected the monster is sleeping as you approached it.\nKill him in %d turns or it shall awaken and slaughter you.\n"
 .balign 4
 bossTurn: .asciz "%d/%d TURN\n"
+.balign 4
+scanBoss: "%d"
+
+.balign 4
+bWinMess: .asciz "\nCongratulations, you have beaten the boss and have won the game!\n"
+.balign 4
+bLossMess: .asciz "\nUnfortunately you have not beaten the boss. Better luck next time.\n"
 
 
-
+.balign 4
+inputBoss: .word 0
 .balign 4
 returnBoss: .word 0
 
@@ -24,21 +32,46 @@ main:
 
 	//LDR R7, address_of_numOfRounds
 	//LDR R7, [R7]
-	MOV R7, #7
+
+	MOV R9, #100 //Bosses Health
 for_loop_boss:
+	MOV R2, #7
+	MOV R7, #7
 	ADD R8, R8, #1
+	MOV R1, R8
 
 	LDR R0, address_of_bossTurn
 	bl printf
 
+	LDR R0, address_of_scanBoss
+	LDR R1, address_of_inputBoss
+	bl scanf
+
+	SUB R9, R9, #13
+	CMP R9, #0
+		BLE checkWinBoss
+
 	CMP R7, R8
 		BEQ checkWinBoss
-		BLT for_loop_boss
+		bl for_loop_boss
 
 
 checkWinBoss:
+	CMP R9, #0
+		BLE bossWIN
+		BGT bosLOSS
 
-_exit:
+bossWIN:
+	LDR R0, address_of_bWinMess
+	bl printf
+	bl _exitBoss
+
+bossLOSS:
+	LDR R0, address_of_bLossMess
+	bl printf
+	bl _exitBoss
+
+_exitBoss:
 	LDR LR, address_of_returnBoss
 	LDR LR, [LR]
 	BX LR
@@ -46,7 +79,7 @@ _exit:
 
 address_of_PromptBoss: .word PromptBoss
 address_of_returnBoss: .word returnBoss
-address_of_bossBoss: .word bossTurn
+address_of_bossTurn: .word bossTurn
 
 //External
 .global printf
