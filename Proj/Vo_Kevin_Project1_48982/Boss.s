bbe985ff476731f42497dcde4373e6ca18b1c00b
root <root@raspberrypi.(none)>
2015-11-08 06:23:30 -0800

    tg

---
+++ Proj/Vo_Kevin_Project1_48982/Boss.s
@@ -0,0 +1,137 @@
+//Data
+.data
+.balign 4
+PromptBoss: .asciz "\nAs expected the monster is sleeping as you approached it.\nKill it before it wakes up and slaughters you.\n"
+.balign 4
+bossWarn: .asciz "Warning: You must enter 'a' to attack. If you entered in something else, it will count as a miss and no damage will be done.\n"
+.balign 4
+bossTurn: .asciz "\n                     %d/%d TURN\n"
+.balign 4
+scanBoss: .asciz "%s"
+.balign 4
+disbossHealth: .asciz "Boss' Health: %d"
+.balign 4
+bossInstr: .asciz "\n\nEnter 'a' to attack: "
+
+.balign 4
+bWinMess: .asciz "\nCongratulations, you have beaten the boss and have won the game!\n"
+.balign 4
+bLossMess: .asciz "\nUnfortunately you have not beaten the boss. Better luck next time.\n"
+.balign 4
+missMess: .asciz "\n**** You missed! ****\n"
+
+.balign 4
+inputBoss: .word 0
+.balign 4
+returnBoss: .word 0
+
+.balign 4
+bosscheckA: .word 'a'
+
+.text
+	.global bossBattle
+bossBattle:
+	LDR R1, address_of_returnBoss
+	STR LR, [R1]
+
+	LDR R0, address_of_PromptBoss
+	bl printf
+
+	LDR R0, address_of_bossWarn
+	bl printf
+
+	MOV R8, #0 //#setting number of rounds
+
+	MOV R9, #100 //Bosses Health
+
+for_loop_boss:
+	MOV R2, R6 //For displaying
+	MOV R7, R6 //For comparison
+	ADD R8, R8, #1
+	MOV R1, R8
+
+	//Prints the incr/total turn to fight boss
+	LDR R0, address_of_bossTurn
+	bl printf
+
+	//Displays boss' health
+	MOV R1, R9
+	LDR R0, address_of_disbossHealth
+	bl printf
+
+	//Displays instruction to player
+	LDR R0, address_of_bossInstr
+	bl printf
+
+	//Takes in input to attack BOSS
+	LDR R0, address_of_scanBoss
+	LDR R1, address_of_inputBoss
+	bl scanf
+
+	LDR R1, address_of_inputBoss
+	LDR R1, [R1]
+
+	LDR R2, address_of_bosscheckA
+	LDR R2, [R2]
+
+	//Checks if input == 'a'
+	CMP R1, R2
+		SUBEQ R9, R9, R5 // only does damage if input == 'a'
+		BNE DisplayMissed //misses if != 'a'
+
+//Checks if boss is still alive and if turn is up
+firstCheck:
+	CMP R9, #0
+		BLE checkWinBoss
+
+	CMP R7, R8
+		BEQ checkWinBoss
+		bl for_loop_boss
+
+//alerts player if they've missed
+DisplayMissed:
+	LDR R0, address_of_missMess
+	bl printf
+	bl firstCheck
+
+//checks if person wins or lose
+checkWinBoss:
+	CMP R9, #0
+		BLE bossWIN
+		BGT bossLOSS
+
+//Displays message if won
+bossWIN:
+	LDR R0, address_of_bWinMess
+	bl printf
+	bl _exitBoss
+
+//displays lose message
+bossLOSS:
+	LDR R0, address_of_bLossMess
+	bl printf
+	bl _exitBoss
+
+_exitBoss:
+	LDR LR, address_of_returnBoss
+	LDR LR, [LR]
+	BX LR
+
+
+address_of_PromptBoss: .word PromptBoss
+address_of_bossTurn: .word bossTurn
+address_of_disbossHealth: .word disbossHealth
+address_of_scanBoss: .word scanBoss
+address_of_bWinMess: .word bWinMess
+address_of_bLossMess: .word bLossMess
+address_of_inputBoss: .word inputBoss
+address_of_returnBoss: .word returnBoss
+address_of_bosscheckA: .word bosscheckA
+address_of_bossInstr: .word bossInstr
+address_of_bossWarn: .word bossWarn
+address_of_missMess: .word missMess
+
+
+//External
+.global printf
+.global scanf
---
+++ Proj/Vo_Kevin_Project1_48982/Pat.s
@@ -0,0 +1,115 @@
+//Data
+.data
+.balign 4
+promptPat: .asciz "\n\nFigure out which number goes next to win a weapon.\n"
+.balign 4
+chestTop: .asciz "\n*******************\n*------[ o ]------*\n*"
+.balign 4
+chestPat: .asciz "%d,"
+.balign 4
+blank: .asciz "_?_\n"
+.balign 4
+chestBot: .asciz "\n*******************\n"
+.balign 4
+returnPat: .word 0
+.balign 4
+scanPat: .asciz "%d"
+.balign 4
+inChest: .word 0
+.balign 4
+askPat: .asciz "Input the next value: "
+.balign 4
+patCorM: .asciz "\nThe input was correct. You've earned a LUPARA +10 Damage.\n"
+.balign 4
+patWro: .asciz "\nSorry, but input should = %d. The prize will NOT be given.\n"
+.text
+	.global Pattern
+Pattern:
+	LDR R1, address_of_returnPat
+	STR LR, [R1]
+
+	LDR R0, address_of_promptPat
+	bl printf
+	LDR R0, address_of_chestTop
+	bl printf
+
+	MOV R1, #14
+	LDR R0, address_of_chestPat
+	bl printf
+	//Pattern sequence
+	//R1 = 2+(R4 + 3)*4
+	//R4++
+
+	//R5 = R4 + 3
+	//R5 *= 4
+	//R1 = R5 + 2
+	MOV R4, #0
+for_loop_pat:
+	ADD R5, R4, #4
+	MOV R5, R5, LSL#2
+	ADD R5, R5, #2
+
+	MOV R1, R5
+	CMP R4, #4
+		BLT printPat
+check:
+	ADD R4, R4, #1
+	CMP R4, #4
+		BLE for_loop_pat
+		BGT inputPat
+printPat:
+	LDR R0, address_of_chestPat
+	bl printf
+	bl check
+inputPat:
+	LDR R0, address_of_blank
+	bl printf
+	LDR R0, address_of_chestBot
+	bl printf
+
+	LDR R0, address_of_askPat
+	bl printf
+
+	LDR R0, address_of_scanPat
+	LDR R1, address_of_inChest
+	bl scanf
+
+	LDR R0, address_of_inChest
+	LDR R0, [R0]
+
+checkInput:
+	CMP R0, R5
+		BEQ corPat
+		BNE wroPat
+corPat:
+	MOV R9, #123 //Correct code
+	LDR R0, address_of_patCorM
+	bl printf
+	BAL _exitPat
+
+wroPat:
+	MOV R9, #111//Incorrect code
+	MOV R1, R5
+	LDR R0, address_of_patWro
+	bl printf
+	BAL _exitPat
+
+_exitPat:
+	LDR LR, address_of_returnPat
+	LDR LR, [LR]
+	BX LR
+
+address_of_patWro: .word patWro
+address_of_patCorM: .word patCorM
+address_of_askPat: .word askPat
+address_of_blank: .word blank
+address_of_promptPat: .word promptPat
+address_of_chestTop: .word chestTop
+address_of_chestPat: .word chestPat
+address_of_chestBot: .word chestBot
+address_of_returnPat: .word returnPat
+address_of_scanPat: .word scanPat
+address_of_inChest: .word inChest
+//External
+.global printf
+.global scanf
